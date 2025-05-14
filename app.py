7fdd5f359e04b4547d82810ad54b49d60fcbd526
eastgrand <m.voldeck@gmail.com>
2025-05-14 17:25:27 -0400

    duplicates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 
+
 # --- EARLY LOGGING SETUP (fixes NameError: logger not defined) ---
 import os
 import sys
@@ -21,11 +22,7 @@ import uuid
 from collections import defaultdict
 
 # --- FLASK APP SETUP (must come after imports) ---
-CORS_ENABLED = True
 app = Flask(__name__)
-if CORS_ENABLED:
-    from flask_cors import CORS
-    CORS(app)
 CORS(app)
 
 # --- /analyze GET handler (added for friendly error) ---
