77acc28639def89583d8263de3d3db241f89cfa3
eastgrand <m.voldeck@gmail.com>
2025-06-19 18:20:25 -0400

    import errors

--- map_nesto_data.py
+++ map_nesto_data.py
@@ -1,1 +1,17 @@
-# Python mapping placeholder
+import pandas as pd
+import numpy as np
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+TARGET_VARIABLE = 'CONVERSION_RATE'
+
+MASTER_SCHEMA = {}
+
+def load_and_preprocess_data():
+    return pd.DataFrame({'ID': [1], 'CONVERSION_RATE': [0.5]})
+
+def initialize_schema(df):
+    pass
+
+FIELD_MAPPINGS = MASTER_SCHEMA
